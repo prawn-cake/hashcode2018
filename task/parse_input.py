65932083a51872359c6f55dcad09513dc47d2d48
Evans Mungai <evans.mungai@f-secure.com>
2018-03-01 21:49:33 +0200

    parsed input file

--- task/parse_input.py
+++ task/parse_input.py
@@ -1,5 +1,41 @@
 """Parse input data methods"""
 
+import sys
+import os
+
 
 def parse():
-    pass
+    """
+    Parse input
+    returns list meta as below
+    R – number of rows of the grid (1 ≤ R ≤ 10000)
+    C – number of columns of the grid (1 ≤ C ≤ 10000)
+    F – number of vehicles in the fleet (1 ≤ F ≤ 1000)
+    N – number of rides (1 ≤ N ≤ 10000)
+    B – per-ride bonus for starting the ride on time (1 ≤ B ≤ 10000)
+    T – number of steps in the simulation (1 ≤ T ≤ 10 )
+
+    returns list of ride tuples as below
+    (a,b) (x,y) (s,f)
+    a – the row of the start intersection (0 ≤ a < R)
+    b – the column of the start intersection (0 ≤ b < C)
+    
+    x – the row of the finish intersection (0 ≤ x < R)
+    y – the column of the finish intersection (0 ≤ y < C)
+    
+    s – the earliest start(0 ≤ s < T)
+    f – the latest finish (0 ≤ f ≤ T) , (f ≥ s + |x − a| + |y − b|)
+    """
+    data = os.path.abspath(sys.argv[1])
+    print(data)
+
+    with open(data) as fp:
+        meta = fp.readline().split()
+        rides = []
+        for l in fp.readlines():
+            l = l.split()
+            if len(l) >= 6:
+                r = (l[0],l[1]), (l[2],l[3]), (l[4],l[5])
+                rides.append(r)
+    
+    return meta, rides
