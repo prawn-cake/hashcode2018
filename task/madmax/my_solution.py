9059eed9b12952e3e43a8a081776668b7a65bbbe
Maksim Ekimovskii <ekimovsky.maksim@gmail.com>
2018-03-06 09:30:27 +0200

    34,863,075 solution

--- task/madmax/my_solution.py
+++ task/madmax/my_solution.py
@@ -1,33 +1,112 @@
-from task import parse_input
+import time
+from task import parse_input, parse_output
 from task.madmax import helpers
-from collections import namedtuple
+from collections import namedtuple, deque
 
 
-Ride = namedtuple('Ride', ['id', 'coord_start', 'coord_finish', 'start_t', 'finish_t'])
-Car = namedtuple('Ride', ['id'])
+Ride = namedtuple('Ride', ['id', 'coord_start', 'coord_finish', 'start_t', 'finish_t', 'dist'])
+Order = namedtuple('Order', ['ride', 'actual_start_t', 'actual_end_t'])
 
 
-def main():
-    meta, rides = parse_input.parse('../../data/b_should_be_easy.in')
+class Car:
+    def __init__(self, idx, t=0):
+        self.id = idx
+        self.t = t
+
+    def __repr__(self):
+        return 'Car(%s, %s)' % (self.id, self.t)
+
+
+def main(meta, rides):
     rows, columns, vehicles, num_of_rides, bonus, T = meta
     print('rows: %s, columns: %s, vehicles: %s, num_of_rides: %s, bonus: %s, T: %s' % (rows, columns, vehicles, num_of_rides, bonus, T))
 
-    rides_list = [Ride(i, item[0], item[1], item[2][0], item[2][1])
+    rides_list = [Ride(i, item[0], item[1], item[2][0], item[2][1], helpers.distance(item[0], item[1]))
                   for i, item in enumerate(rides)]
-    cars_list = [Car(i) for i in range(vehicles)]
-    cur_time = 10000
+    cars_list = [Car(i, 0) for i in range(vehicles)]
 
-    # print(sorted(rides_list, key=lambda r: r.start_t))
-    priority_queue = sorted([(ride_priority(cur_time, r), r) for r in rides_list], reverse=True)
-    print('priority_queue: %s' % priority_queue)
-    ride = priority_queue[0]
+    schedule = {car: [] for car in cars_list}
 
     # To track cars
     radar = {car.id: (0, 0) for car in cars_list}
 
-    # Main loop
-    # - pick the ride with the higher priority
-    # - find the car based on the score
+    # TODO: this could be reordered
+    # priority_queue = deque(sorted(rides_list, key=lambda r: r.start_t))
+    priority_queue = sorted(rides_list, key=lambda r: r.start_t)
+    print('priority_queue: %s' % priority_queue)
+    total_distance = sum([ride.dist for ride in rides_list])
+    avg_distance_per_car = total_distance / vehicles
+
+    total_score = 0
+
+    total_ride_cost = 0
+    total_bonus = 0
+    skipped_rides = 0
+
+    for ride in priority_queue:
+        if total_score > T:
+            break
+
+        picked_car = None
+        min_cost = None
+
+        for car in cars_list:  # TODO: is car available
+            # if sum([o.ride.dist for o in schedule[car]]) >= avg_distance_per_car:
+            #     continue
+
+            if not is_car_available(car, ride, schedule):
+                continue
+
+            time_to_reach = helpers.distance(radar[car.id], ride.coord_start)
+            time_to_wait = max(0, ride.start_t - car.t - time_to_reach)
+            ride_distance = helpers.distance(ride.coord_start, ride.coord_finish)
+
+            actual_start_t = car.t + time_to_reach + time_to_wait
+            actual_end_t = actual_start_t + ride_distance
+
+            if actual_end_t >= ride.finish_t:
+                # a car can't make it
+                continue
+
+            cost = time_to_reach + time_to_wait + ride_distance
+
+            if len(schedule[car]) == 0:
+                picked_car, min_cost = car, cost
+                break
+            elif min_cost is None:
+                min_cost, picked_car = cost, car
+            elif cost < min_cost:
+                picked_car = car
+                min_cost = cost
+
+        if picked_car is None:
+            skipped_rides += 1
+            continue
+
+        # Add an order
+        total_score += ride_score(total_ride_cost, picked_car, ride, radar, bonus)
+
+        schedule[picked_car].append(Order(ride, actual_start_t, actual_end_t))
+        radar[picked_car.id] = ride.coord_finish
+        total_ride_cost += min_cost
+        picked_car.t += min_cost
+
+    print('skipped: %d' % skipped_rides)
+    return schedule
+
+
+def is_car_available(car, ride, schedule):
+    orders = schedule[car]
+    if not orders:
+        return True
+
+    # NOTE: we assume that orders ARE ordered by time
+    last_order = orders[-1]
+
+    # Check if we can fit the order to the schedule
+    if (last_order.actual_end_t + ride.dist) < ride.finish_t:
+        return True
+    return False
 
 
 def ride_score(cur_time, car, ride, radar, bonus=0):
@@ -35,7 +114,7 @@ def ride_score(cur_time, car, ride, radar, bonus=0):
 
     time_to_reach = helpers.distance(car_coord, ride.coord_start)
     time_to_wait = max(0, ride.start_t - cur_time - time_to_reach)
-    ride_distance = helpers.distance(ride.coord_start, ride.coord_finish)
+    ride_distance = ride.dist
 
     actual_start_time = cur_time + time_to_reach + time_to_wait
     actual_end_time = actual_start_time + ride_distance
@@ -51,17 +130,43 @@ def ride_score(cur_time, car, ride, radar, bonus=0):
     return ride_distance + bonus
 
 
-def ride_priority(cur_time, ride):
-    """Ride score priority regarding the current moment in time
+def ride_cost(car, ride, radar):
+    car_coord = radar[car.id]
 
-    """
-    ttw = max(0, ride.start_t - cur_time)  # time to wait to start
-    distance = helpers.distance(ride.coord_start, ride.coord_finish)
+    time_to_reach = helpers.distance(car_coord, ride.coord_start)
+    time_to_wait = max(0, ride.start_t - car.t - time_to_reach)
+    ride_distance = ride.dist
 
-    if cur_time >= ride.finish_t:
-        return 0
-    return distance - ttw
+    actual_start_t = car.t + time_to_reach + time_to_wait
+    actual_end_t = actual_start_t + ride_distance
+
+    cost = time_to_reach + time_to_wait + ride_distance
+    return cost, actual_start_t, actual_end_t
+
+
+def generate_result(filename):
+    meta, rides = parse_input.parse('../../data/%s' % filename)
+    t0 = time.time()
+    schedule = main(meta, rides)
+    print('Elapsed {:.4f}s'.format(time.time() - t0))
+
+    items = []
+    for car, orders in sorted(schedule.items(), key=lambda item: item[0].id):
+        items.append([str(o.ride.id) for o in orders])
+        print('car %d -> %s' % (car.id, ', '.join([str(o.ride.id) for o in orders])))
+
+    parse_output.write_output(items, '../../result/%s.txt' % filename)
 
 
 if __name__ == '__main__':
-    main()
+    # meta, rides = parse_input.parse('../../data/a_example.in')
+    names = ['a_example.in', 'b_should_be_easy.in', 'c_no_hurry.in',
+             'd_metropolis.in', 'e_high_bonus.in']
+    # filename = 'a_example.in'
+    filename = 'b_should_be_easy.in'
+    # filename = 'c_no_hurry.in'
+    # filename = 'd_metropolis.in'
+    # filename = 'e_high_bonus.in'
+    for filename in names:
+        generate_result(filename)
+    # generate_result(filename)
--- task/parse_output.py
+++ task/parse_output.py
@@ -8,3 +8,9 @@ def output(orders: list, output_file: str = "output.txt"):
                     f.write(str(ride))
                 else:
                     f.write(" " + str(ride))
+
+
+def write_output(items, output_file):
+    with open(output_file, 'w') as fd:
+        for orders in items:
+            fd.write('%s %s\n' % (len(orders), ' '.join(orders)))
