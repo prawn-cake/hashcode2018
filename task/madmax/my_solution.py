e7c9200977cfb57d94f6715b9a4298eb00e39ba8
Maksim Ekimovskii <ekimovsky.maksim@gmail.com>
2018-03-11 13:05:05 +0200

    35,167,476 solution

--- task/madmax/my_solution.py
+++ task/madmax/my_solution.py
@@ -51,17 +51,36 @@ def pick_car(ride, car_list, schedule, radar, avg_distance_per_car, reserved_car
     return picked_car, actual_start_t, actual_end_t, min_cost, reserved_cars
 
 
-def prioritise_rides(rides_list):
+def prio_by_start_t(rides_list):
     # Default implementation to sort by start time
     return sorted(rides_list, key=lambda r: r.start_t)
 
 
-def main(meta, rides):
+def prio_by_start_t_and_dist(rides_list):
+    return sorted(rides_list, key=lambda r: (r.start_t, helpers.distance((0, 0), r.coord_start)))
+
+
+def prio_by_distance(rides_list):
+    return sorted(rides_list, key=lambda r: r.dist)
+
+
+def prio_by_finish_t(rides_list):
+    return sorted(rides_list, key=lambda r: r.finish_t)
+
+
+def main(meta, rides, prioritise_rides=prio_by_start_t):
     rows, columns, vehicles, num_of_rides, bonus, T = meta
     print('rows: %s, columns: %s, vehicles: %s, num_of_rides: %s, bonus: %s, T: %s' % (rows, columns, vehicles, num_of_rides, bonus, T))
 
     rides_list = [Ride(i, item[0], item[1], item[2][0], item[2][1], helpers.distance(item[0], item[1]))
                   for i, item in enumerate(rides)]
+
+    # for ride in rides_list:
+    #     print(ride)
+
+    total_distance = sum([ride.dist for ride in rides_list])
+    min_rate_to_skip = 1 - ((T * vehicles) / total_distance)
+    print('Min accepted rate of skipped rides: {:.2f} %'.format(min_rate_to_skip * 100))
     cars_list = [Car(i, 0) for i in range(vehicles)]
 
     schedule = {car: [] for car in cars_list}
@@ -70,14 +89,10 @@ def main(meta, rides):
     radar = {car.id: (0, 0) for car in cars_list}
 
     # TODO: this could be reordered
-    # priority_queue = deque(sorted(rides_list, key=lambda r: r.start_t))
-    priority_queue = sorted(rides_list, key=lambda r: r.start_t)
-    # print('priority_queue: %s' % priority_queue)
-    total_distance = sum([ride.dist for ride in rides_list])
+    priority_queue = prioritise_rides(rides_list)
     avg_distance_per_car = total_distance / vehicles
 
     total_score = 0
-
     total_ride_cost = 0
     total_bonus = 0
     skipped_rides = 0
@@ -108,15 +123,28 @@ def main(meta, rides):
 
         schedule[picked_car].append(Order(ride, actual_start_t, actual_end_t))
         radar[picked_car.id] = ride.coord_finish
+
         total_ride_cost += min_cost
         picked_car.t += min_cost
 
         if actual_start_t == ride.start_t:
             total_bonus += bonus
 
-    print('skipped: %d (%d percent)' % (skipped_rides, (skipped_rides / num_of_rides) * 100))
+    print('skipped: {} ({} %)'.format(skipped_rides, (skipped_rides / num_of_rides) * 100))
     print('total score: %d (score: %d, bonus: %d)' % (total_score + total_bonus, total_score, total_bonus))
     return schedule
+
+
+def is_car_busy(car, schedule, current_t, cost):
+    orders = schedule[car]
+    if not orders:
+        return False
+
+    last_order = orders[-1]
+    if (last_order.start_t - current_t) > cost:
+        # We have some room to move
+        return True
+    return False
 
 
 def is_car_available(car, ride, schedule):
@@ -128,9 +156,9 @@ def is_car_available(car, ride, schedule):
     last_order = orders[-1]
 
     # Check if we can fit the order to the schedule
-    if (last_order.actual_end_t + ride.dist) < ride.finish_t:
-        return True
-    return False
+    res = [(order.actual_end_t + ride.dist) < ride.finish_t for order in orders]
+    return all(res)
+    # return False
 
 
 def ride_score(cur_time, car, ride, radar, bonus=0):
@@ -168,10 +196,10 @@ def ride_cost(car, ride, radar):
     return cost, actual_start_t, actual_end_t
 
 
-def generate_result(filename):
+def generate_result(filename, prio_fn):
     meta, rides = parse_input.parse('../../data/%s' % filename)
     t0 = time.time()
-    schedule = main(meta, rides)
+    schedule = main(meta, rides, prioritise_rides=prio_fn)
     print('Elapsed {:.4f}s'.format(time.time() - t0))
 
     items = []
@@ -184,15 +212,21 @@ def generate_result(filename):
 
 if __name__ == '__main__':
     # meta, rides = parse_input.parse('../../data/a_example.in')
-    names = ['a_example.in', 'b_should_be_easy.in', 'c_no_hurry.in',
-             'd_metropolis.in', 'e_high_bonus.in']
+    names = [
+        # file, rides_prio function
+        ('a_example.in', prio_by_start_t),
+        ('b_should_be_easy.in', prio_by_start_t),
+        ('c_no_hurry.in', prio_by_distance),
+        ('d_metropolis.in', prio_by_finish_t),
+        ('e_high_bonus.in', prio_by_start_t)
+    ]
     # filename = 'a_example.in'
     # filename = 'b_should_be_easy.in'
     # filename = 'c_no_hurry.in'
     # filename = 'd_metropolis.in'
     # filename = 'e_high_bonus.in'
-    for filename in names:
+    for filename, prio_fn in names:
         print('-' * 100)
         print('Generate result for %s' % filename)
-        generate_result(filename)
-    # generate_result(filename)
+        generate_result(filename, prio_fn)
+    # generate_result(filename, prio_by_finish_t)
